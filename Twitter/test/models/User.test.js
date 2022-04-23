cff5b474236f8c8cf96c8e813a4f33f66e26ec4b
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-22 20:09:53 -0600

    pasando pruebas y terminando requerimiento 4

--- Twitter/test/models/User.test.js
+++ Twitter/test/models/User.test.js
@@ -18,4 +18,13 @@ describe("Unit Test for User class", () => {
         expect(user.getDateCreated).not.toBeUndefined()
         expect(user.getLastUpdated).not.toBeUndefined()
     })
+
+    test('Add setters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
+    })
 })
