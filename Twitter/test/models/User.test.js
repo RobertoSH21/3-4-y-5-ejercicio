ac9177f7af8557efe860d86b650c6a76f83ec1e9
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-22 19:39:22 -0600

    viendo error de la prueba sin clase

---
+++ Twitter/test/models/User.test.js
@@ -0,0 +1,13 @@
+const User = require('./../../app/models/User')
+
+describe("Unit Test for User class", () => {
+    test('Create an User object', () => {
+        const user = new User(1, "carlogilamr", "Carlo", "Bio", "dateCreated", "lastUpdated")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    })
+})
