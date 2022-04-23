d25e371582913b0446c278833bdf55191faa06c6
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-22 19:43:03 -0600

    pasando la prueba y el requerimiento 1

--- Twitter/test/models/User.test.js
+++ Twitter/test/models/User.test.js
@@ -2,7 +2,7 @@ const User = require('./../../app/models/User')
 
 describe("Unit Test for User class", () => {
     test('Create an User object', () => {
-        const user = new User(1, "carlogilamr", "Carlo", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
         expect(user.id).toBe(1)
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
